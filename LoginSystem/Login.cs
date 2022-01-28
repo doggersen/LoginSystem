56720cefdfc179a1bc80af98771a5cbf0a475dac
doggersen <doggersen@gmail.com>
2022-01-28 23:08:15 +0100

    Add project files.

---
+++ LoginSystem/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    internal class Login
+    {
+        public void GreetUser()
+        {
+
+        }
+    }
+}
---
+++ LoginSystem/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginSystem
+{
+    internal class User
+    {
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+        public bool IsAdministrator { get; set; }
+
+        public static List<User> users = new List<User>();
+       
+        
+    }
+}
