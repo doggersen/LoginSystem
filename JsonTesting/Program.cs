994dcfd1c3f0de87d26a317fa073e2af82dc6cf5
doggersen <martin.mbj.johansen@gmail.com>
2022-02-01 00:51:49 +0100

    almost working i t hink

--- JsonTesting/Program.cs
+++ JsonTesting/Program.cs
@@ -1,4 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text.Json;
+
+
+
+List<string> list = new List<string>();
+list.Add("martin");
+
+
+var jsonString = JsonSerializer.Serialize(list);
+
+string fileName = "ListOfNames.json";
+//string jsonString = JsonSerializer.Serialize(weatherForecast);
+File.WriteAllText(fileName, jsonString);
+
+
+AddItemToList();
+
+void AddItemToList()
+{
+    Console.WriteLine("write names, write 1 to exit");
+    while (true)
+    {
+        if ("1" == Console.ReadLine())
+        {
+            break;
+        }
+        else
+        {
+            list.Add(Console.ReadLine());
+        }
+    }
+
+    ShowFullList();
+}
+
+void ShowFullList()
+{
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+Console.ReadKey();
+
+
 
 
