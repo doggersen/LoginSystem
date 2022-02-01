9538a90dca8613d38a59f7b81452c7209090f415
doggersen <martin.mbj.johansen@gmail.com>
2022-02-02 00:28:57 +0100

    json projekt virker nu

--- JsonTesting/Program.cs
+++ JsonTesting/Program.cs
@@ -4,35 +4,78 @@ using System.Text.Json;
 
 
 List<string> list = new List<string>();
-list.Add("martin");
+
+MainMenu();
 
 
-var jsonString = JsonSerializer.Serialize(list);
-
-string fileName = "ListOfNames.json";
-//string jsonString = JsonSerializer.Serialize(weatherForecast);
-File.WriteAllText(fileName, jsonString);
-
-
-AddItemToList();
-
-void AddItemToList()
+void MainMenu()
 {
-    Console.WriteLine("write names, write 1 to exit");
-    while (true)
+    Console.WriteLine("Press 1: add names to list");
+    Console.WriteLine("Press 2: show current list");
+    Console.WriteLine("Press 3: save current list to Json-file");
+    Console.WriteLine("Press 4: load json list");
+
+    switch (Console.ReadLine())
     {
-        if ("1" == Console.ReadLine())
-        {
+        case "1":
+            AddItemToList(Console.ReadLine());
             break;
-        }
-        else
-        {
-            list.Add(Console.ReadLine());
-        }
+        case "2":
+            ShowFullList();
+            break;
+        case "3":
+            SaveListToJson();
+            break;
+        case "4":
+            LoadListFromJson();
+            break;
+    }
+}
+
+
+
+//AddItemToList();
+
+void AddItemToList(string name)
+{
+    list.Add(name);
+    Console.WriteLine("add more things to list? Press '1' for yes, '2' for no.");
+    string userInput = Console.ReadLine();
+    if (userInput == "1")
+    {
+        Console.Write($"write next name for the list: ");
+        AddItemToList(Console.ReadLine());
+    }
+    else if (userInput == "2")
+    {
+        MainMenu();
     }
 
-    ShowFullList();
 }
+void AddItemToListTEST()
+{
+
+    Console.WriteLine("write names, write 1 to exit");
+    while (Console.ReadLine() != "1")
+    {
+        // userInput = Console.ReadLine();
+        //if ("1" == Console.ReadLine())
+        //{
+        //    MainMenu();
+        //    break;
+        //}
+        //else
+        //{
+        //    list.Add(Console.ReadLine());
+        //}
+
+        list.Add(Console.ReadLine());
+    }
+    MainMenu();
+}
+
+   
+
 
 void ShowFullList()
 {
@@ -40,6 +83,36 @@ void ShowFullList()
     {
         Console.WriteLine(item);
     }
+    Console.WriteLine("press any key to return to main menu");
+    Console.ReadKey();
+    MainMenu();
+}
+
+void SaveListToJson()
+{
+    var jsonString = JsonSerializer.Serialize(list);
+
+    string fileName = "ListOfNames.json";
+    //string jsonString = JsonSerializer.Serialize(weatherForecast);
+    File.WriteAllText(fileName, jsonString);
+
+    Console.Clear();
+    Console.WriteLine("list now saved to json, press any key to return to main menu");
+    Console.ReadKey();
+    MainMenu();
+}
+
+
+void LoadListFromJson()
+{
+    string fileName = "ListOfNames.json";
+    var jsonString = File.ReadAllText(fileName);
+    list = JsonSerializer.Deserialize<List<string>>(jsonString);
+
+    Console.WriteLine("list now loaded from json file. press any key to return to menu");
+    Console.ReadKey();
+    MainMenu();
+
 }
 
 Console.ReadKey();
